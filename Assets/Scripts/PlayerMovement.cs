0d01c7308f84e074b363c06c3a99b054cbaf7898
sharprod8 <spervez25@elam.co.uk>
2026-06-16 12:41:13 +0100

    Started movement, imported assets

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed = 5f;
+
+    private float horizontalMovement;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        rb.linearVelocity = new Vector2(horizontalMovement * moveSpeed, rb.linearVelocity.y);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontalMovement = context.ReadValue<Vector2>().x;
+    }
+}

1412abecee6c058820b253a6394cf7f2b635a3df
sharprod8 <spervez25@elam.co.uk>
2026-06-23 12:47:02 +0100

    Wave Manager

--- Assets/Scripts/Waves/WaveManager.cs
+++ Assets/Scripts/Waves/WaveManager.cs
@@ -11,21 +11,65 @@ public class WaveManager : MonoBehaviour
     private TextMeshProUGUI waveCounterUItext;
     public Transform screenCenter;
     public Transform waveCounterPos;
-    
+
+    private Vector3 doubleScale = new Vector3(2, 2, 1);
+    private Vector3 halfScale = new Vector3(1, 1, 1);
+    private bool waveCounterCentered;
+    private bool RunningOpeningWaveCounterAnimation;
+
     private void Start()
     {
         waveCounterCG.alpha = 0f;
         waveCounter = 1;
         waveCounterUItext = waveCounterUI.GetComponentInChildren<TextMeshProUGUI>();
-        waveCounterUI.transform.position = screenCenter.transform.position;
+
+        ResetWaveCounterPosition();
 
         PlayOpeningWaveCounterAnimation();
     }
 
     private void PlayOpeningWaveCounterAnimation()
     {
-        LeanTween.alphaCanvas(waveCounterCG, 1, 1).setEase(LeanTweenType.easeOutQuint).setOnComplete(() =>
         
-        );
+        ResetWaveCounterPosition();
+
+        if (waveCounterCentered)
+        {
+            LeanTween.scale(waveCounterUI, doubleScale, 0.1f);
+
+            LeanTween.alphaCanvas(waveCounterCG, 1, 1).setEase(LeanTweenType.easeOutQuint).setOnComplete(() =>
+            {
+                LeanTween.scale(waveCounterUI, halfScale, 0.5f);
+                LeanTween.move(waveCounterUI, waveCounterPos, 1).setEase(LeanTweenType.easeInOutCirc).setOnComplete(() =>
+                {
+                    RunningOpeningWaveCounterAnimation = false;
+                }
+                );
+            }
+            );
+        }
+    }
+
+    void ResetWaveCounterPosition()
+    {
+        waveCounterUI.transform.position = screenCenter.transform.position;
+
+        waveCounterCentered = true;
+    }
+
+    public void AdvanceWave()
+    {
+        if (RunningOpeningWaveCounterAnimation)
+            Debug.Log("Didn't advance wave, wait for animation first");
+            return;
+        
+        waveCounter += 1;
+        StartWave();
+    }
+
+    public void StartWave()
+    {
+        PlayOpeningWaveCounterAnimation();
+        RunningOpeningWaveCounterAnimation = true;
     }
 }
